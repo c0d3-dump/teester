fcd0a83b2017b71ed1a7bf0b47212328579e5e1a
c0d3-dump <c0d3.dump@gmail.com>
2023-05-13 18:35:10 +0530

    created jsondb and rest api with go for saving testing data in json format

--- teester.go
+++ teester.go
@@ -1,7 +1,86 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func readJson(filename string) (string, error) {
+	bytes, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return string(bytes[:]), err
+}
+
+func writeJson(filename string, data string) error {
+	err := os.WriteFile(filename, []byte(data), fs.ModePerm)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return err
+}
+
+type PostBody struct {
+	Data string `json:"data"`
+}
+
+func fileCheck(filename string) error {
+	conn, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("data.json does not exists!")
+		conn, err := os.Create(filename)
+		conn.WriteString("{}")
+		conn.Close()
+		return err
+	}
+	conn.Close()
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello")
+	dataFileName := "data.json"
+	err := fileCheck(dataFileName)
+	if err != nil {
+		fmt.Println("unable to create file!")
+		return
+	}
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, there!")
+	})
+
+	app.Get("/getData", func(c *fiber.Ctx) error {
+		data, err := readJson(dataFileName)
+		if err != nil {
+			return c.SendStatus(404)
+		}
+
+		var jsonStruct map[string]interface{}
+		json.Unmarshal([]byte(data), &jsonStruct)
+
+		return c.JSON(jsonStruct)
+	})
+
+	app.Post("/postData", func(c *fiber.Ctx) error {
+		var body PostBody
+		json.Unmarshal(c.Body(), &body)
+
+		err := writeJson(dataFileName, body.Data)
+		if err != nil {
+			return c.SendStatus(404)
+		}
+		return c.SendStatus(201)
+	})
+
+	log.Fatal(app.Listen(":3333"))
 }
